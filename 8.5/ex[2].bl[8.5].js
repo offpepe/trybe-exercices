8b629fd9adad602d4d8f2939e1cf7cc349b65688
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-04-07 20:58:13 -0400

    add ex[1].bl[8.5]

---
+++ 8.5/ex[2].bl[8.5].js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+const sum = ((...numbers) => numbers.length !== 0 ? numbers.reduce((x,y) => x + y ) : 0);
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
